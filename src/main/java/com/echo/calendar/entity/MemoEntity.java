135514f45eb97be65f085fe1a295cfd414eae9d6
JackeyTower <1046968088@qq.com>
2021-04-01 18:16:06 +0800

    初始化

---
+++ src/main/java/com/echo/calendar/entity/MemoEntity.java
@@ -0,0 +1,11 @@
+package com.echo.calendar.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemoEntity {
+}
